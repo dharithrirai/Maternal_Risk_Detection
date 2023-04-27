1b43691e042aa71457075259da7ee0a4f9518f59
dharithrirai <dharithrirai18@gmail.com>
2023-04-27 15:10:47 +0530

    Commit 3

--- Backend/ML/LSTM/Predict.py
+++ Backend/ML/LSTM/Predict.py
@@ -9,16 +9,14 @@ with open('Backend/ML/LSTM/scaler.pkl', 'rb') as f:
 # Load the TFLite model
 interpreter = tf.lite.Interpreter(model_path="model.tflite")
 interpreter.allocate_tensors()
-
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 # Create an input array
-x = np.array([[28, 120, 80, 110, 38.8, 75]])
+x = np.array([[25,140,100,122.39999999999999,36.666666666666664,80]])
 
 # Scale the input data using the loaded scaler object
 x = scaler.transform(x)
-
 # Reshape the input array to match the input shape of the TFLite model
 x = x.astype(np.float32) 
 x = x.reshape((x.shape[0], 1, x.shape[1]))
--- Backend/ML/LSTM/create_model.py
+++ Backend/ML/LSTM/create_model.py
@@ -1,4 +1,3 @@
-
 import numpy as np 
 from sklearn.preprocessing import MinMaxScaler
 from keras.utils import to_categorical
@@ -15,6 +14,7 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.utils import to_categorical
 import tensorflow.lite as lite
 import pickle
+from imblearn.over_sampling import SMOTE
 
 data = pd.read_csv('Backend/ML/LSTM/New_Maternal.csv')
 
@@ -27,12 +27,18 @@ x=scaler.fit_transform(x)
 with open('Backend/ML/LSTM/scaler.pkl', 'wb') as f:
     pickle.dump(scaler, f)
 
-y=to_categorical(y)
-
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+
+# Apply SMOTE to balance the dataset
+smote = SMOTE(random_state=42)
+x_train, y_train = smote.fit_resample(x_train, y_train)
+print(y_train)
+
 x_train=x_train.reshape((x_train.shape[0],1,x_train.shape[1]))
 x_test=x_test.reshape((x_test.shape[0],1,x_test.shape[1]))
 print(x_test.shape[0],1,x_test.shape[1])
+# Get the number of unique values in y
+num_classes = len(np.unique(y))
 
 model = Sequential()
 model.add(LSTM(units=64, input_shape=(1, x_train.shape[2]), return_sequences=True))
@@ -40,7 +46,8 @@ model.add(Dropout(0.2))
 model.add(LSTM(units=32, return_sequences=True))
 model.add(Dropout(0.2))
 model.add(LSTM(units=16))
-model.add(Dense(units=y.shape[1], activation='softmax'))
+model.add(Dense(units=num_classes, activation='softmax'))
+
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
@@ -49,11 +56,11 @@ model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y
 _, accuracy = model.evaluate(x_test, y_test)
 print("Accuracy:", accuracy)
 
-model.save("model.h5")
-m = keras.models.load_model("model.h5")
+model.save("Backend/ML/LSTM/model.h5")
+m = keras.models.load_model("Backend/ML/LSTM/model.h5")
 converter = lite.TFLiteConverter.from_keras_model(m)
 converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS, tf.lite.OpsSet.SELECT_TF_OPS]
 converter._experimental_lower_tensor_list_ops = False
 tflite_model = converter.convert()
-with open('model.tflite', 'wb') as f:
+with open('Backend/ML/LSTM/model.tflite', 'wb') as f:
     f.write(tflite_model)
